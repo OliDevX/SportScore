c5f560a7b4ddc3891f47051aeb3a5ce7d0050f21
OliDevelopments <armando.ibarraran@gmail.com>
2016-04-17 18:53:32 -0500

    gamecontrol class is updated and Display.java is created

---
+++ app/src/main/java/com/olidevx/sportscore/Display.java
@@ -0,0 +1,16 @@
+package com.olidevx.sportscore;
+
+/**
+ * Created by Armando on 17/04/2016.
+ */
+public class Display {
+    int mDigit_1;
+    int mDigit_2;
+    int mDigit_3;
+    int mScore_points;
+
+    public void UpdateDisplay () {
+
+    }
+
+}
--- app/src/main/java/com/olidevx/sportscore/gamecontrol.java
+++ app/src/main/java/com/olidevx/sportscore/gamecontrol.java
@@ -4,5 +4,23 @@ package com.olidevx.sportscore;
  * Created by Armando on 17/04/2016.
  */
 public class gamecontrol {
+    team mTeam1;
+    team mTeam2;
+    Display mDisplay_1;
+    Display mDisplay_2;
 
+    public void setGame (int pointsTeam1, int pointsTeam2, String nameTeam1, String nameTeam2) {
+        mTeam1.setTeamName(nameTeam1);
+        mTeam2.setTeamName(nameTeam2);
+        mTeam1.setTeamPoints(pointsTeam1);
+        mTeam2.setTeamPoints(pointsTeam2);
+    }
+
+    public void change_score (team mTeam, boolean up_or_down) {
+        if (up_or_down) {
+            mTeam.increasePoint();
+        } else {
+            mTeam.decreasePoint();
+        }
+    }
 }
